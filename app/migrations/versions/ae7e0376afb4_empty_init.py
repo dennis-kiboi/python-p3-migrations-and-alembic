34ee6512024ccf1253f5629fee5faa09a3d1ecb6
Dennis Kiboi <dennis.kiboi@moringaschool.com>
2024-01-20 19:10:00 +0300

    Completed Assignment

---
+++ app/migrations/versions/ae7e0376afb4_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: ae7e0376afb4
+Revises: 
+Create Date: 2024-01-20 18:17:44.953885
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'ae7e0376afb4'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
